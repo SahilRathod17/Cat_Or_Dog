f92326f9d9785d5c33329f63b3283e8290439faf
iamSahil <samael17111@gmail.com>
2023-01-28 21:01:33 +0530

    Complete

---
+++ lib/constant/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color colorFF = const Color(0xFFFFFFFF);
+  static Color color54 = const Color(0xFF545454);
+  static Color color3B = const Color(0xFFFCD13B);
+  static Color colorD9 = const Color(0xFFD9D9D9);
+  static Color color6F = const Color(0xFF65696F);
+}
---
+++ lib/constant/images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static String splash = "assets/images/splash-removebg-preview.png";
+  static String home = "assets/images/splash-removebg-preview.png";
+}
